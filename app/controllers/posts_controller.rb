1f261e2929b1e67ce142eff7456a63897ef03144
Shohei Takeda <lucky.take.star@gmail.com>
2023-04-22 18:36:54 +0900

    index,new,createアクションとビューの作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,20 @@
 class PostsController < ApplicationController
   def index
+    @posts = Post.all
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    @post = Post.new(params.require(:post).permit(:title, :start_date, :end_date, :all_day, :memo))
+    if @post.save
+      flash[:notice] = "スケジュールを新規登録しました"
+      redirect_to posts_path
+    else
+      render "new"
+    end
   end
 
   def show
@@ -19,5 +28,5 @@ class PostsController < ApplicationController
 
   def destroy
   end
-  
+
 end
