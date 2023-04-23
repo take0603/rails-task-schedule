bc24f71daa97e8b8db12fe5f8b8fb742504bfcc1
Shohei Takeda <lucky.take.star@gmail.com>
2023-04-23 19:27:02 +0900

    バリデーション設定、エラーメッセージの表示（部分テンプレート作成）

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,13 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {maximum: 20}
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  validates :memo, length: {maximum: 500}
+
+  validate :end_date_not_backdate
+
+  def end_date_not_backdate
+    errors.add(:end_date, "は開始日以降の日付を選択してください") if self.end_date != nil && self.end_date < self.start_date
+  end
+
 end
