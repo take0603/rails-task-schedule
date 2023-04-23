54fc7902da7fe11ecc5be1d55e01e5ba27021483
Shohei Takeda <lucky.take.star@gmail.com>
2023-04-23 15:54:48 +0900

    show,edit,update,destoryアクション追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,15 +18,28 @@ class PostsController < ApplicationController
   end
 
   def show
+    @post = Post.find(params[:id])
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def update
+    @post = Post.find(params[:id])
+    if @post.update(params.require(:post).permit(:title, :start_date, :end_date, :all_day, :memo))
+      flash[:notice] = "スケジュールを更新しました"
+      redirect_to :posts
+    else
+      render "edit"
+    end
   end
 
   def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    flash[:notice] = "スケジュールを削除しました"
+    redirect_to :posts
   end
 
 end
